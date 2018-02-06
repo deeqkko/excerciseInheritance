11d4333120e2b1e6d1b38f998e7497a76f5174b6
deeqkko <kimmo.koivusalo@gmail.com>
2018-02-06 10:07:13 +0200

    2.1 tehty

---
+++ src/excerciseinheritance/Kulkuneuvo.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package excerciseinheritance;
+
+/**
+ *
+ * @author hamkTeam4/deeqkko
+ */
+public class Kulkuneuvo extends Auto {
+    
+}
